2effc000a02c46d8f724ec06ed42d63ff2aed7cf
Andrea Cassotti <cassottiandrea@hotmail.com>
2024-05-01 14:53:03 +0200

    Connections grouped in a function

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,9 @@
 #include "Client.h"
 
 
+void createConnections(Server &server, Client &client);
+
+
 int main(int argc, char **argv)
 {
     QGuiApplication app(argc, argv);
@@ -23,11 +26,17 @@ int main(int argc, char **argv)
     // https://scythe-studio.com/en/blog/how-to-integrate-qml-and-c-expose-object-and-register-c-class-to-qml
     engine.rootContext()->setContextProperty("client", &client);
 
-    QObject::connect(&server, &Server::sendMsgToClient, &client, &Client::receiveMsgFromServer);
-    QObject::connect(&client, &Client::sendMsgToServer, &server, &Server::receiveMsgFromClient);
-    QObject::connect(&client, &Client::askWifiList, &server, &Server::sendWifiList);
+    createConnections(server, client);
 
     client.connectToServer();
 
     return app.exec();
+}
+
+
+void createConnections(Server &server, Client &client)
+{
+    QObject::connect(&server, &Server::sendMsgToClient, &client, &Client::receiveMsgFromServer);
+    QObject::connect(&client, &Client::sendMsgToServer, &server, &Server::receiveMsgFromClient);
+    QObject::connect(&client, &Client::askWifiList, &server, &Server::sendWifiList);
 }
